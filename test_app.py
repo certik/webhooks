f863ee1654064fadd4805bf1da3b9ee0b8e2934a
Ondrej Certik <ondrej@certik.cz>
2009-07-12 02:07:26 -0600

    test fixed

--- test_app.py
+++ test_app.py
@@ -1,6 +1,10 @@
 import sys
+import os
+import urllib
+import json
 url = sys.argv[1]
 
-import os
-data = "birthyear=1905&press=%20OK%20"
+d = {23: 23, "ok": 2}
+data = json.dumps(d)
+data = "payload=%s" % urllib.quote(data)
 os.system('curl -d "%s" %s' % (data, url))
