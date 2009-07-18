bcfb08d4296ca06b20296c426695e0f24a944982
Ondrej Certik <ondrej@certik.cz>
2009-07-17 19:32:17 -0600

    Use templates + some simple db

--- hooks/models.py
+++ hooks/models.py
@@ -1,3 +1,10 @@
+from appengine_django.models import BaseModel
 from django.db import models
 
-# Create your models here.
+class User(BaseModel):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+
+class Repository(BaseModel):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User)
--- hooks/views.py
+++ hooks/views.py
@@ -3,15 +3,21 @@ import pprint
 import logging
 
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 from django.utils import simplejson
+
+from models import User
 
 def index(request):
     if request.method == 'GET':
-        return HttpResponse("Hooks.")
+        return render_to_response("hooks/index.html")
     elif request.method == 'POST':
         payload = request.POST["payload"]
         payload = urllib.unquote(payload)
         payload = simplejson.loads(payload)
         logging.info("-"*40 + "\n" + pprint.pformat(payload) + "\n" + "-"*40)
-        #print("-"*40 + "\n" + pprint.pformat(payload) + "\n" + "-"*40)
+        repository = payload["repository"]
+        owner = repository["owner"]
+        u = User(name=owner["name"], email=owner["email"])
+        u.save()
         return HttpResponse("OK\n")
