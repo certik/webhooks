c45cbf90f64398a9461d108b4ea4e63c9a069ce9
Ondrej Certik <ondrej@certik.cz>
2009-07-17 21:26:56 -0600

    another test added

--- hooks/tests.py
+++ hooks/tests.py
@@ -45,3 +45,57 @@ class HookTest(TestCase):
         assert len(list(users_list)) == 1
         assert users_list[0].name == "user4"
         assert users_list[0].email == "some4@at.com"
+
+    def test_hook2(self):
+        d = {
+          "before": "5aef35982fb2d34e9d9d4502f6ede1072793222d",
+          "repository": {
+            "url": "http://github.com/defunkt/github",
+            "name": "github",
+            "description": "You're lookin' at it.",
+            "watchers": 5,
+            "forks": 2,
+            "private": 1,
+            "owner": {
+              "email": "chris@ozmm.org",
+              "name": "defunkt"
+            }
+          },
+          "commits": [
+            {
+              "id": "41a212ee83ca127e3c8cf465891ab7216a705f59",
+              "url": "http://github.com/defunkt/github/commit/41a212ee83ca127e3c8cf465891ab7216a705f59",
+              "author": {
+                "email": "chris@ozmm.org",
+                "name": "Chris Wanstrath"
+              },
+              "message": "okay i give in",
+              "timestamp": "2008-02-15T14:57:17-08:00",
+              "added": ["filepath.rb"]
+            },
+            {
+              "id": "de8251ff97ee194a289832576287d6f8ad74e3d0",
+              "url": "http://github.com/defunkt/github/commit/de8251ff97ee194a289832576287d6f8ad74e3d0",
+              "author": {
+                "email": "chris@ozmm.org",
+                "name": "Chris Wanstrath"
+              },
+              "message": "update pricing a tad",
+              "timestamp": "2008-02-15T14:36:34-08:00"
+            }
+          ],
+          "after": "de8251ff97ee194a289832576287d6f8ad74e3d0",
+          "ref": "refs/heads/master"
+        }
+
+        data = simplejson.dumps(d)
+        response = self.client.post('/', {"payload": data})
+        assert response.status_code == 200
+        assert response.content == "OK\n"
+
+        response = self.client.get('/hooks/users/')
+        assert response.status_code == 200
+        users_list = response.context["users_list"]
+        assert len(list(users_list)) == 1
+        assert users_list[0].name == "defunkt"
+        assert users_list[0].email == "chris@ozmm.org"
