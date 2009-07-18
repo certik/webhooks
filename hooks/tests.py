c4a1880e78729ec3a01a45b7cc4a6ebaaa2f6ed0
Ondrej Certik <ondrej@certik.cz>
2009-07-17 18:41:07 -0600

    Use assert

--- hooks/tests.py
+++ hooks/tests.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 class SimpleTest(TestCase):
     def test_hooks(self):
         response = self.client.get('/hooks/')
-        self.failUnlessEqual(response.status_code, 200)
+        assert response.status_code == 200
 
     def test_index(self):
         response = self.client.get('/')
-        self.failUnlessEqual(response.status_code, 200)
+        assert response.status_code == 200
